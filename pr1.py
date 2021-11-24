254ac9b6094af0c66c70c4dfd160ce2ec09fa2c6
Akshatlakhara <85992585+Akshatlakhara@users.noreply.github.com>
2021-11-24 11:48:09 +0530

    Add files via upload
    This is a python based Dice simulator project which randomly generates dice outputs the number of times its rolled. Help is taken from udemy 2021 bootcamp.

---
+++ pr1.py
@@ -0,0 +1,44 @@
+# Dice simulator
+
+import random
+ans = str(input("continue for y and n to exit"))
+while ans == "y":
+    x = random.randint(1, 6)
+
+    if x == 1:
+        print("---------")
+        print("|       |")
+        print("|   0   |")
+        print("|       |")
+        print("---------")
+    elif x == 2:
+        print("---------")
+        print("|   0   |")
+        print("|       |")
+        print("|   0   |")
+        print("---------")
+    elif x == 3:
+        print("---------")
+        print("|   0   |")
+        print("|   0   |")
+        print("|   0   |")
+        print("---------")
+    elif x == 4:
+        print("---------")
+        print("| 0   0 |")
+        print("|       |")
+        print("| 0   0 |")
+        print("---------")
+    elif x == 5:
+        print("---------")
+        print("| 0   0 |")
+        print("|   0   |")
+        print("| 0   0 |")
+        print("---------")
+    elif x == 6:
+        print("---------")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("| 0   0 |")
+        print("---------")
+    ans = str(input("continue for y and n to exit"))
